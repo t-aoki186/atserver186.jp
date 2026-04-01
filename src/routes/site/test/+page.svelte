49ecfcd961ab0319c6fb87767f4283e4ec4a408d
taoki <84th.tccaoki@gmail.com>
2026-04-01 15:08:38 +0900

    add: github actions動作確認用にテストページを追加

---
+++ src/routes/site/test/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	const { data } = $props();
+
+	import { onMount } from 'svelte';
+	import { reveal } from '$lib/reveal';
+
+	let pageTitle = 'テスト';
+</script>
+
+<svelte:head>
+	<title>{pageTitle} | {data.site_title}</title>
+    <meta property="og:title" content="{pageTitle} | {data.site_title}" />
+</svelte:head>
+
+<main class="mt-15 mr-1 ml-1 min-h-screen">
+	<div class="container m-auto mt-25 border-b-2 border-b-(--main-text-color)">
+		<p class="tf26-page-title" style="color: black; margin-bottom: 0;">{pageTitle}</p>
+	</div>
+	<section class="container mx-auto mt-25 mb-25">
+		<div class="flex flex-col">
+            <p class="mb-2">これはテストページです。</p>
+        </div>
+	</section>
+</main>
+<ol class="main-breadcrumb container mx-auto">
+	<li><a href="/">ホーム</a></li>
+	<li>{pageTitle}</li>
+</ol>
