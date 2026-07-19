8534be6128be39d1b132cc18b43ed2b3dd2b5ca0
taoki <84th.tccaoki@gmail.com>
2026-07-20 08:18:59 +0900

    feat: リンクページの内容を最新に

--- src/routes/site/links/+page.svelte
+++ src/routes/site/links/+page.svelte
@@ -17,15 +17,6 @@
 			class="mx-auto mt-8 flex max-w-117 flex-col items-center rounded-2xl bg-white px-8 py-8"
 			style="box-shadow: 0 2px 16px rgba(0, 0, 0, 0.07);"
 		>
-			<!--
-			<div class="profile-title">
-				<img
-					src="../img/ATSLink_light.png"
-					alt="ATSLink"
-					style="width:100px;max-width:90vw;height:auto;margin-bottom:8px;"
-				/>
-			</div>
-			-->
 			<img
 				class="profile-avatar mb-2.5 h-auto w-27.5 rounded-full object-cover"
 				style="border: 2px solid #e6e6e6;"
@@ -37,50 +28,32 @@
 				<div class="flex flex-col">
 					<p class="tf26-dialog-title">SNSリンク</p>
 				</div>
-				<a class="profile-link-card" href="https://x.com/atshift186_xz1" target="_blank">
+				<a class="profile-link-card" href="https://x.com/t_aoki186" target="_blank">
 					<i class="fa-brands fa-x-twitter text-xl"></i>
-					X<br /><span>@ATShift186_XZ1</span></a
+					X<br /><span>@t_aoki186</span></a
 				>
+				<!--
 				<a class="profile-link-card" href="https:/msk.atserver186.jp/@ats" target="_blank"
 					><i class="tf26-icon-material icon-misskey-mi text-xl"></i>
 					Misskey(あっとみすき)<br /><span>@ats</span></a
 				>
-				<a
-					class="profile-link-card"
-					href="https://commina.atserver186.jp/ats/"
-					target="_blank"
+				<a class="profile-link-card" href="https://commina.atserver186.jp/ats/" target="_blank"
 					><i class="fa-solid fa-up-right-from-square text-xl"></i>
 					Commina<br /><span>@ats</span></a
 				>
+				-->
 				<a class="profile-link-card" href="https://github.com/t-aoki186" target="_blank"
 					><i class="fa-brands fa-github text-xl"></i>
 					Github<br /><span>@t-aoki186</span></a
 				>
-				<a class="profile-link-card" href="https://www.youtube.com/@atshift" target="_blank"
-					><i class="fa-brands fa-youtube text-xl"></i>
-					YouTube<br /><span>@ATShift</span></a
+				<a class="profile-link-card" href="https://gitlab.atserver186.jp/t-aoki186" target="_blank"
+					><i class="fa-brands fa-gitlab text-xl"></i>
+					Gitlab(atserver)<br /><span>@t-aoki186</span></a
 				>
-				<div class="flex flex-col">
-					<p class="tf26-dialog-title">相互リンク</p>
-				</div>
-				<!--
-				<div
-					style="display:flex;flex-direction:column;align-items:center;justify-content:center;margin-top:24px;"
+				<a class="profile-link-card" href="/site/mulinks"
+					><i class="fa-solid fa-link text-xl"></i>
+					相互リンク<br /></a
 				>
-					<a href="https://atserver186.jp" target="_blank"
-						><img
-							src="../img/ATSLink_light.png"
-							alt="ATSLink"
-							style="width:70px;max-width:90vw;height:auto;margin-bottom:8px;"
-						/></a
-					><a
-						href="https://link.atserver186.jp"
-						style="font-size:0.8rem;color:black;text-decoration:none;margin-bottom:4px;"
-						target="_blank">Created by link.atserver186.jp</a
-					>
-					<p style="font-size:0.8rem;margin:0;">©︎link.atserver186.jp All Rights Reserved</p>
-				</div>
-				-->
 			</div>
 		</div>
 	</section>
